9a016a3ee7951ad959d91d7b1a40d82955fb4481
Christian Holzberger <ch@mosaik-software.de>
2016-06-06 01:11:26 +0200

    Cleanup, use grunt logging

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Grunt PDF Optimizer using Ghostscript
 
 You need to have ghostscript installed on your system to use this grunt-plugin.
+# PDF Optimize
+
+See http://milan.kupcevic.net/ghostscript-ps-pdf/
+https://gist.github.com/gpoulter/2351254
+
--- src/pdf-optimize.js
+++ src/pdf-optimize.js
@@ -1,11 +1,18 @@
+/**
+ * @author Christian Holzberger <ch at mosaiksoftware.de> 
+ * @license MIT
+ * Copyright 2016 MOSAIK Software 
+ */
 var cmd = [
     "-dNOPAUSE",
     "-dBATCH",
-    "-sDEVICE=pdfwrite"
+    "-sDEVICE=pdfwrite",
+		"-dDetectDuplicateImages=true",
+		"-dCompressFonts=true"
 ];
 
 var default_options = {
-  gs_command: "gs"
+  gs_command: "gs",
   quality: "ebook",
   compatibility_level: "1.4"
 }
@@ -14,9 +21,18 @@ var shell = require("shelljs");
 var _ = require("underscore");
 var fs = require("fs"); 
 
+module.exports.quality = {
+	SCREEN: "screen", // best
+	EBOOK: "ebook",
+	PRINTER: "printer",
+	PREPRESS: "prepress",
+	DEFAULT: "default" // worst
+};
+
+
 module.exports = function(grunt) {
     grunt.registerMultiTask('pdf-optimize', function() {
-        grunt.subhead("Running pdf-optimize");
+        grunt.log.subhead("Running pdf-optimize");
         var count = 0;
         var size_before = 0;
         var size_after = 0;
@@ -24,12 +40,12 @@ module.exports = function(grunt) {
         var options = this.options (default_options);
         _.each(this.files, function(file) {
             if ( !grunt.file.isFile(file.src[0])) {
-                console.error("Input File does not exist: " + file.src[0]);
+                grunt.log.error("Input File does not exist: " + file.src[0]);
                 return;
             }
 
             if ( grunt.file.isFile(file.dest)) {
-                console.log("Output file " + file.dest + " allready exists skipping");
+                grunt.log.writeln("Output file allready exists skipping");
                 return;
             }
 
@@ -41,29 +57,37 @@ module.exports = function(grunt) {
             run.push("-sOutputFile=" + file.dest);
             run.push(file.src[0]);
             var rcmd = options.gs_command + " " + run.join(" ");
-            //console.log(process.cwd());
-            //console.log("Optimizing " + file.src[0]);
-            //console.log("Running " + rcmd);
-
-
             var ret = shell.exec(rcmd, {
                 silent: true
             });
             if (ret) {
-                count++;
-                size_before += fs.statSync(file.src[0]);
-                size_after += fs.statSync(dest);
+                var file_size_before = fs.statSync(file.src[0]).size;
+                var file_size_after = fs.statSync(file.dest).size;
 
-                grunt.ok("Optimized " + file.src[0]);
+								var ratio = (file_size_after / file_size_before).toFixed(2);
+								if ( ratio > 0.9 ) {
+									fs.unlinkSync(file.dest)
+									fs.linkSync(file.src[0], file.dest);
+									
+									grunt.log.ok("Could not optimize " + file.src[0]);
+								} else {
+									// stats
+                	count++;
+									size_before += file_size_before;
+									size_after += file_size_after;
+									
+                	grunt.log.ok("Optimized " + file.src[0] + " [x"+ratio+"]");
+								}
             } else {
-                grunt.console.error("Failed optimizing " + file.src[0]);
+                grunt.log.error("Failed optimizing " + file.src[0]);
             }
 
-            grunt.subhead("Optimized " + count + " files");
-            grunt.log("Size before: " + (size_before / 1024 / 1024) + " MiB");
-            grunt.log("Size after: " + (size_after / 1024 / 1024) + "MiB");
-            grunt.log("Saved: " + ((size_after - size_before) / 1024 / 1024) + "MiB");
-
+            
         });
+				grunt.log.subhead("Optimized " + count + " files");
+        grunt.log.writeln("Size before: " + (size_before / 1024 / 1024) + " MiB");
+        grunt.log.writeln("Size after: " + (size_after / 1024 / 1024) + "MiB");
+        grunt.log.writeln("Saved: " + ((size_after - size_before) / 1024 / 1024) + "MiB");
+
     });
 };
